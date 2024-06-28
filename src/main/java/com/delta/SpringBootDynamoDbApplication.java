e3c7e552aeba5db03d2e21995814cd0c8198e4e0
Harshal-A-Bhagwat <108969994+Harsh21111993@users.noreply.github.com>
2024-06-28 16:33:10 +0530

    Update SpringBootDynamoDbApplication.java

--- src/main/java/com/delta/SpringBootDynamoDbApplication.java
+++ src/main/java/com/delta/SpringBootDynamoDbApplication.java
@@ -9,7 +9,6 @@ import com.delta.config.CreateTable;
 public class SpringBootDynamoDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDynamoDbApplication.class, args);
 
 		SpringApplication app = new SpringApplication(SpringBootDynamoDbApplication.class);
 		app.addListeners(new CreateTable());
